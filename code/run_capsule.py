24ee744f8b0b8e4e422409070a7caa1aee493f62
arielleleon <54869022+arielleleon@users.noreply.github.com>
2025-03-06 13:20:22 -0800

    feat: use nwb utils library
    * Combine ophys, eye tracking and behavior nwbs.
    
    * Trim down code a bit.
    
    * Added datasets.json, environment.json, resources.json; and changes in 3 other files
    
    * Edited datasets.json, run_capsule.py
    
    * Lint.
    
    * Bug in writing final nwb.
    
    * Correct save path to append data.
    
    * Determine save strategy.
    
    * Clean up code and add temporary files.
    
    * feat: add aind-nwb-utils library
    
    * fix: use aind-nwb-utils
    
    * feat: adding required libraries and app panel
    
    * fix: adding libraries

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,48 @@
-""" top level run script """
+"""top level run script"""
+
+import logging
+import shutil
+from pathlib import Path
+
+from aind_nwb_utils.utils import combine_nwb_file
+from hdmf_zarr import NWBZarrIO
+from pydantic_settings import BaseSettings
+from pynwb import NWBHDF5IO
+
+
+class NWBCombineSettings(
+    BaseSettings, cli_parse_args=True, cli_ignore_unknown_args=True
+):
+    """Settings for NWBCombine"""
+
+    input_dir: str = "/data"
+    output_dir: str = "/results"
+    output_format: str = "zarr"
+
 
 def run():
-    """ basic run function """
-    pass
+    """basic run function"""
+    combine_settings = NWBCombineSettings()
+    input_dir = Path(combine_settings.input_dir)
+    output_dir = Path(combine_settings.output_dir)
+    ophys_fp = next(input_dir.glob("ophys/*.nwb"))
+    behavior_fp = next(input_dir.glob("behavior/*.nwb"))
+    eye_fp = next(input_dir.glob("eye_tracking/*.nwb"))
+    save_io = NWBZarrIO
+    if combine_settings.output_format.lower() == "hdf5":
+        save_io = NWBHDF5IO
 
-if __name__ == "__main__": run()
+    logging.info(
+        "Combining NWB files, %s, %s and %s", ophys_fp, behavior_fp, eye_fp)
+    for idx, nwb_fp in enumerate([behavior_fp, eye_fp]):
+        if idx == 0:
+            output_fp = combine_nwb_file(ophys_fp, nwb_fp, save_io)
+        else:
+            output_fp = combine_nwb_file(output_fp, nwb_fp, save_io)
+
+    shutil.move(output_fp, output_dir / "session.nwb")
+    logging.info("Done")
+
+
+if __name__ == "__main__":
+    run()
