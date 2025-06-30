02050ce1f567be759b4f6dfe48b8d7f37f12e7d7
arielleleon <ariellel@alleninstitute.org>
2025-06-30 19:51:01 +0000

    Edited datasets.json, run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,5 +1,7 @@
 """top level run script"""
 
+from datetime import datetime as dt
+import json
 import logging
 import shutil
 from pathlib import Path
@@ -28,20 +30,22 @@ def run():
     output_dir = Path(combine_settings.output_dir)
     ophys_fp = next(input_dir.glob("ophys/*.nwb"))
     behavior_fp = next(input_dir.glob("behavior/*.nwb"))
-    eye_fp = next(input_dir.glob("eye_tracking/*.nwb"))
+    data_desc_fp = next(input_dir.rglob("data_description.json"))
+    with open(data_desc_fp) as j:
+        name = json.load(j)["name"]
+    name = name.split("processed")[0][:-1] + "_processed_" + dt.now().strftime("%y-%m-%d_%H-%M-%S") + ".nwb"
     save_io = NWBZarrIO
     if combine_settings.output_format.lower() == "hdf5":
         save_io = NWBHDF5IO
 
     logging.info(
-        "Combining NWB files, %s, %s and %s", ophys_fp, behavior_fp, eye_fp)
-    for idx, nwb_fp in enumerate([behavior_fp, eye_fp]):
+        "Combining NWB files, %s, %s and %s", ophys_fp, behavior_fp)
+    for idx, nwb_fp in enumerate([behavior_fp]):
         if idx == 0:
             output_fp = combine_nwb_file(ophys_fp, nwb_fp, save_io)
-        else:
-            output_fp = combine_nwb_file(output_fp, nwb_fp, save_io)
+        
 
-    shutil.move(output_fp, output_dir / "session.nwb")
+    shutil.move(output_fp, output_dir / name)
     logging.info("Done")
 
 
